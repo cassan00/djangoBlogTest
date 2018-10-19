3a4196520f7b86bb41834a21b07dc74698e1c221
cassan <cassan00@protonmail.com>
2018-10-19 23:04:04 +0900

    장고걸스 블로그 작업물 빽업

---
+++ blog/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+from django.shortcuts import render, get_object_or_404
+from .forms import PostForm
+from django.shortcuts import redirect
+
+# Create your views here.
+
+def post_list(request):
+    # posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')  # 쿼리셋 변수
+    posts = Post.objects.all()
+    print(posts.values_list('text'))
+    print("-쿼리:"+str(posts.query));
+    print(posts) # 객체들의 출력이 나온다. models.py 에 __str__ 로 title을 찍도록 되어 있으므로
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        #form = PostForm(request.POST, instance=post)
+        form = PostForm(instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
+
